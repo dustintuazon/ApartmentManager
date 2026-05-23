20e08145db60b650f883dea3a6fb84c38b613097
dstn <dustuazon@gmail.com>
2026-05-23 22:29:45 +0800

    Added dashboard items

--- ApartmentManager/Controllers/HomeController.cs
+++ ApartmentManager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using ApartmentManager.Data;
 using ApartmentManager.Models;
+using ApartmentManager.ViewModels.Dashboard;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,14 +12,27 @@ namespace ApartmentManager.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly AppDbContext _context;
+        private readonly UserManager<User> _userManager;
+        public HomeController(AppDbContext context, UserManager<User> userManager)
         {
-
+            _context = context;
+            _userManager = userManager;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = _userManager.GetUserId(User);
+            var dashboardViewModel = new DashboardViewModel
+            {
+                YearIncome = await _context.Transactions.Where(t => t.Date.Year == DateTime.Now.Year && t.UserId == userId).SumAsync(t => t.Amount),
+                MonthIncome = await _context.Transactions.Where(t => t.Date.Month == DateTime.Now.Month && t.Date.Year == DateTime.Now.Year && t.UserId == userId).SumAsync(t => t.Amount),
+                RoomCount = await _context.Rooms.Where(r=>r.UserId == userId).CountAsync(),
+                TenantCount = await _context.Tenants.Where(t => t.UserId == userId && (t.Status == Status.Settled || t.Status == Status.MovingOut)).CountAsync(),
+                AvailableRooms = await _context.Rooms.Where(r => r.IsAvailable && r.UserId == userId).CountAsync()
+            };
+            return View(dashboardViewModel);
         }
 
         public IActionResult Privacy()
